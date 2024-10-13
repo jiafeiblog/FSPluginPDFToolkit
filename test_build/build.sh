9a793b979b1df0d3e6e1cc81c9116a16942a7a2b
jiafei <jiafeiblog@163.com>
2024-10-13 08:38:56 +0000

    feat:init

--- test_build/build.sh
+++ test_build/build.sh
@@ -4,7 +4,8 @@
 # pwd=2BfxNjLPinz6ah2F
 project_name="feishu-pdf-batch-merger"
 version="latest"
-cd ../ && pnpm install && pnpm run build
+# cd ../ && pnpm install && pnpm run build
+cd ../
 cd test_build && mv ../dist/ .
 
 docker buildx build  --platform linux/amd64 -t $project_name:$version .
